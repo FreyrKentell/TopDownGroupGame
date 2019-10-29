67421251cf018abe297369967f6770b1311858c3
Isaac Ortiz <19.isaac.ortiz@ksd.org>
2019-10-29 12:43:52 -0700

    I finished the Audio Manager Script-Isaac

---
+++ TopDown Group Game/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+using System;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sound;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        foreach (Sound s in sound)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+
+    void Start ()
+    {
+        Play("Theme");
+    }
+
+    public void Play (string name)
+    {
+        Sound s = Array.Find(sound, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound" + name + " not found!");
+            return;
+        }
+            
+        s.source.Play();
+    }
+}
