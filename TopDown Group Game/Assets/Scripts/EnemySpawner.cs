c83c712a7d1dfd73856952372cb67431f97978de
Isaac Ortiz <19.isaac.ortiz@ksd.org>
2019-11-01 12:08:47 -0700

    Finshed Enemy Spawner Script-Isaac

---
+++ TopDown Group Game/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public float currentTimer = 0.0f;
+    public float waveOne = 1.0f;
+    public float waveTwo = 2.0f;
+    public float waveThree = 3.0f;
+    public float waveFour = 4.0f;
+    public GameObject WaveOne;
+    public GameObject WaveTwo;
+    public GameObject WaveThree;
+    public GameObject WaveFour;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTimer += Time.deltaTime;
+        Mathf.RoundToInt(currentTimer);
+        if (currentTimer >= waveOne)
+        {
+            GameObject enemy = Instantiate(WaveOne);
+        } 
+    }
+}
