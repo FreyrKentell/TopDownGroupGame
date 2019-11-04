d197ca25695ab6255987073ea586336a09e650eb
Isaac Ortiz <19.isaac.ortiz@ksd.org>
2019-11-04 11:55:05 -0800

    Fixed Pathing Issue with Enemy Spawner-Isaac

--- TopDown Group Game/Assets/Scripts/EnemyAI.cs
+++ TopDown Group Game/Assets/Scripts/EnemyAI.cs
@@ -15,6 +15,7 @@ public class EnemyAI : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
         startPosition = transform.position;
     }
 
