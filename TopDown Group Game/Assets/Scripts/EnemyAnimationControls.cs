d7fef1781ee964a00d994f753dd5ae3746520fad
Isaac Ortiz <19.isaac.ortiz@ksd.org>
2019-11-04 13:13:05 -0800

    Level Timer Prefab

--- TopDown Group Game/Assets/Scripts/EnemyAnimationControls.cs
+++ TopDown Group Game/Assets/Scripts/EnemyAnimationControls.cs
@@ -5,10 +5,11 @@ using Pathfinding;
 
 public class EnemyAnimationControls : MonoBehaviour
 {
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
