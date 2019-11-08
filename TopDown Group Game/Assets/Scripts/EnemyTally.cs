0c469ac497b18d40f52a64906bda68ae4f5ba7e3
Isaac Ortiz <19.isaac.ortiz@ksd.org>
2019-11-08 13:57:50 -0800

    Enemy Tally Script Started - Isaac

---
+++ TopDown Group Game/Assets/Scripts/EnemyTally.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTally : MonoBehaviour
+{
+    public int enemyTotal = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        FindGameObjectWithTag("Enemy");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
