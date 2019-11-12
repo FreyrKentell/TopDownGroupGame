0ca49c8b3655ba0bff1e997b49ab62f5f549419d
Isaac Ortiz <19.isaac.ortiz@ksd.org>
2019-11-12 12:23:22 -0800

    Enemy Tally

---
+++ TopDown Group Game/Assets/Scripts/EnemyTally.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTally : MonoBehaviour
+{
+    public GameObject Boss;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject[] gameObjects;
+        gameObjects = GameObject.FindGameObjectsWithTag("Enemy");
+
+        if (gameObjects.Length == 0)
+        {
+            Debug.Log("No game objects are tagged with 'Enemy'");
+            GameObject enemy = Instantiate(Boss, transform.position, Quaternion.identity);
+        }
+    }
+}
