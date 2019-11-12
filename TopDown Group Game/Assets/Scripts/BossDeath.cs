7060343496cca787ca8bfd6abc9f830445e814f4
Isaac Ortiz <19.isaac.ortiz@ksd.org>
2019-11-12 12:57:00 -0800

    Boss Death Script

---
+++ TopDown Group Game/Assets/Scripts/BossDeath.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BossDeath : MonoBehaviour
+{
+    public int health = 20;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "PlayerBullet")
+        {
+            health--;
+            if (health <= 0)
+            {
+                SceneManager.LoadScene("WinScene");
+                Destroy(gameObject);
+
+            }
+        }
+    }
+}
